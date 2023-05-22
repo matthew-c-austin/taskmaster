95ca1d965b6f098c4dff18e22122b5af944691cc
Matthew Austin <111619012+matthew-c-austin@users.noreply.github.com>
2023-05-22 15:50:49 -0700

    Create AddTaskActivity

---
+++ app/src/main/java/com/mca/taskmaster/activities/AddTaskActivity.java
@@ -0,0 +1,16 @@
+package com.mca.taskmaster.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mca.taskmaster.R;
+
+public class AddTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+    }
+}
