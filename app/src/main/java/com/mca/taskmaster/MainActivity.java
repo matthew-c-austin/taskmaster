e1dffbbfc2110798910e69a64b3dfb8c44695520
Matthew Austin <111619012+matthew-c-austin@users.noreply.github.com>
2023-05-22 15:57:41 -0700

    Connect MainActivity and AddTaskActivity

--- app/src/main/java/com/mca/taskmaster/MainActivity.java
+++ app/src/main/java/com/mca/taskmaster/MainActivity.java
@@ -2,7 +2,11 @@ package com.mca.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.mca.taskmaster.activities.AddTaskActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button goToAddTaskFormButton = (Button) findViewById(R.id.add_task_activity_button);
+
+        goToAddTaskFormButton.setOnClickListener(v -> {
+            Intent goToAddTaskFormIntent = new Intent(MainActivity.this, AddTaskActivity.class);
+
+            startActivity(goToAddTaskFormIntent);
+        });
     }
 }
--- app/src/main/java/com/mca/taskmaster/activities/AddTaskActivity.java
+++ app/src/main/java/com/mca/taskmaster/activities/AddTaskActivity.java
@@ -3,6 +3,8 @@ package com.mca.taskmaster.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.mca.taskmaster.R;
 
@@ -12,5 +14,11 @@ public class AddTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+        Button addTaskButton = (Button) findViewById(R.id.add_task_button);
+
+        addTaskButton.setOnClickListener(v -> {
+            ((TextView) findViewById(R.id.submitted_text_view)).setText(R.string.submitted_confirmation);
+        });
     }
 }
