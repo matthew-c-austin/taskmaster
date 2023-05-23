44a1c5d124d7c717983a79fb80ecead4fb33dbe2
Matthew Austin <111619012+matthew-c-austin@users.noreply.github.com>
2023-05-23 16:21:09 -0700

    Add TaskDetailActivity

---
+++ app/src/main/java/com/mca/taskmaster/activities/TaskDetailActivity.java
@@ -0,0 +1,16 @@
+package com.mca.taskmaster.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mca.taskmaster.R;
+
+public class TaskDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_detail);
+    }
+}
