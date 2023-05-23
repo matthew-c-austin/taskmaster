5762964dc704ee8ec9a3ca9d09abe0023647a736
Matthew Austin <111619012+matthew-c-austin@users.noreply.github.com>
2023-05-23 16:01:54 -0700

    Update activity_main and add 3 hardcoded buttons

---
+++ app/src/main/java/com/mca/taskmaster/activities/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.mca.taskmaster.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mca.taskmaster.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
