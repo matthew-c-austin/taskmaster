b6fd790f962c815016bed062eceb91e0a30c800c
Matthew Austin <111619012+matthew-c-austin@users.noreply.github.com>
2023-05-23 16:59:44 -0700

    Finalize SettingsActivity hookup

--- app/src/main/java/com/mca/taskmaster/MainActivity.java
+++ app/src/main/java/com/mca/taskmaster/MainActivity.java
@@ -3,19 +3,55 @@ package com.mca.taskmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.mca.taskmaster.activities.AddTaskActivity;
 import com.mca.taskmaster.activities.AllTasksActivity;
+import com.mca.taskmaster.activities.SettingsActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivity";
+
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    setupSettingsButton();
+    setupAddTaskButton();
+    setupAllTasksButton();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String username = preferences.getString(SettingsActivity.USERNAME_TAG, "");
+
+        if (!username.isEmpty()) {
+            String myTasksTitleTextView = username + "'s Tasks";
+            ((TextView) findViewById(R.id.my_tasks_title)).setText(myTasksTitleTextView);
+        }
+    }
+
+    public void setupSettingsButton() {
+        ((FloatingActionButton) findViewById(R.id.settingsButton)).setOnClickListener(v -> {
+            Intent goToSettingsIntent = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(goToSettingsIntent);
+        });
+    }
+
+    public void setupAddTaskButton() {
         Button goToAddTaskFormButton = (Button) findViewById(R.id.add_task_activity_button);
 
         goToAddTaskFormButton.setOnClickListener(v -> {
@@ -23,7 +59,9 @@ public class MainActivity extends AppCompatActivity {
 
             startActivity(goToAddTaskFormIntent);
         });
+    }
 
+    public void setupAllTasksButton() {
         Button goToAllTasksButton = (Button) findViewById(R.id.all_tasks_activity_button);
 
         goToAllTasksButton.setOnClickListener(v -> {
--- app/src/main/java/com/mca/taskmaster/activities/SettingsActivity.java
+++ app/src/main/java/com/mca/taskmaster/activities/SettingsActivity.java
@@ -2,15 +2,51 @@ package com.mca.taskmaster.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.mca.taskmaster.R;
 
 public class SettingsActivity extends AppCompatActivity {
+    public static final String USERNAME_TAG = "username";
+
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        populateUsernameEditText(preferences);
+        setupSaveButton(preferences);
+    }
+
+    public void populateUsernameEditText(SharedPreferences preferences) {
+        String username = preferences.getString(USERNAME_TAG, "");
+        ((EditText) findViewById(R.id.usernameEditText)).setText(username);
+    }
+
+    public void setupSaveButton(SharedPreferences preferences) {
+        Button saveButton = findViewById(R.id.saveSettingsButton);
+        saveButton.setOnClickListener(v -> {
+            // creating an editor because SharedPreferences is read-only
+            SharedPreferences.Editor preferenceEditor = preferences.edit(); // this fails if you declare "preferences" within the onCreate()
+
+            // grabbing the string we want to save from the user input
+            EditText usernameEditText = findViewById(R.id.usernameEditText);
+            String usernameString = usernameEditText.getText().toString();
+
+            // save the string to shared preferences
+            preferenceEditor.putString(USERNAME_TAG, usernameString);
+            preferenceEditor.apply(); // Nothing will happen if you don't do this!
+
+            Toast.makeText(SettingsActivity.this, "Settings saved!", Toast.LENGTH_SHORT).show();
+        });
     }
 }
