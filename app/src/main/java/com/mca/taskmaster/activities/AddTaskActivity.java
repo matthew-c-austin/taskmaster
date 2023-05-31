0cd52be60252878b5789ab52cfebe9b1f00c99d4
Matthew Austin <111619012+matthew-c-austin@users.noreply.github.com>
2023-05-31 16:13:28 -0700

    Update AddTaskActivity

--- app/src/main/java/com/mca/taskmaster/activities/AddTaskActivity.java
+++ app/src/main/java/com/mca/taskmaster/activities/AddTaskActivity.java
@@ -1,41 +1,39 @@
 package com.mca.taskmaster.activities;
 
-import static com.mca.taskmaster.MainActivity.DATABASE_NAME;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import com.amplifyframework.api.graphql.model.ModelMutation;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.generated.model.Task;
+import com.amplifyframework.datastore.generated.model.TaskStatus;
 import com.mca.taskmaster.R;
-import com.mca.taskmaster.models.Task;
-import com.mca.taskmaster.models.TaskStatus;
+import com.mca.taskmaster.utils.TaskStatusUtility;
+
+import java.util.List;
 
 public class AddTaskActivity extends AppCompatActivity {
-//    TaskmasterDatabase taskmasterDatabase;
+    public static final String TAG = "add_task_tag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
 
-//        taskmasterDatabase = Room.databaseBuilder(
-//                        getApplicationContext(),
-//                        TaskmasterDatabase.class,
-//                        DATABASE_NAME)
-//                .allowMainThreadQueries()
-//                .build();
+        List<String> statusList = TaskStatusUtility.getTaskStatusList();
 
         Spinner taskStatusSpinner = findViewById(R.id.spinner_add_task_status);
         taskStatusSpinner.setAdapter(new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_spinner_item,
-                TaskStatus.values()
+                statusList
         ));
 
         Button addTaskButton = (Button) findViewById(R.id.button_add_task_activity_add_task);
@@ -44,13 +42,20 @@ public class AddTaskActivity extends AppCompatActivity {
             EditText titleEditText = findViewById(R.id.my_task_input);
             EditText descriptionEditText = findViewById(R.id.task_description_input);
 
-            Task newTask = new Task(
-                    titleEditText.getText().toString(),
-                    descriptionEditText.getText().toString(),
-                    TaskStatus.fromString(taskStatusSpinner.getSelectedItem().toString())
+            TaskStatus newStatus = TaskStatusUtility.taskStatusFromString(taskStatusSpinner.getSelectedItem().toString());
+
+            Task newTask = Task.builder()
+                    .title(titleEditText.getText().toString())
+                    .body(descriptionEditText.getText().toString())
+                    .status(newStatus)
+                    .build();
+
+            Amplify.API.mutate(
+                    ModelMutation.create(newTask),
+                    success -> Log.i(TAG, "AddTaskActivity.onCreate(): added a task"),
+                    failure -> Log.i(TAG, "AddTaskActivity.onCreate(): failed to add a task")
             );
 
-//            taskmasterDatabase.taskDao().insertTask(newTask);
             ((EditText)findViewById(R.id.my_task_input)).setText("");
             ((EditText)findViewById(R.id.task_description_input)).setText("");
             taskStatusSpinner.setSelection(0);
---
+++ app/src/main/java/com/mca/taskmaster/utils/TaskStatusUtility.java
@@ -0,0 +1,42 @@
+package com.mca.taskmaster.utils;
+
+import com.amplifyframework.datastore.generated.model.TaskStatus;
+
+import java.util.ArrayList;
+
+public class TaskStatusUtility {
+    public static TaskStatus taskStatusFromString(String taskString) {
+        switch (taskString) {
+            case "Complete":
+                return TaskStatus.COMPLETE;
+            case "Assigned":
+                return TaskStatus.ASSIGNED;
+            case "In Progress":
+                return TaskStatus.IN_PROGRESS;
+            default:
+                return TaskStatus.NEW;
+        }
+    }
+
+    public static String taskStatusToString(TaskStatus task) {
+        switch (task) {
+            case COMPLETE:
+                return "Complete";
+            case ASSIGNED:
+                return "Assigned";
+            case IN_PROGRESS:
+                return "In Progress";
+            default:
+                return "New";
+        }
+    }
+
+    public static ArrayList<String> getTaskStatusList() {
+        ArrayList<String> list = new ArrayList<>();
+        for (TaskStatus task : TaskStatus.values()) {
+            list.add(taskStatusToString(task));
+        }
+
+        return list;
+    }
+}
