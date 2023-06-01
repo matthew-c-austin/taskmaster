b5c888481d6c905f8626eeaf5229ac1ec7aa837d
Matthew Austin <111619012+matthew-c-austin@users.noreply.github.com>
2023-06-01 15:10:48 -0700

    Update AddTaskActivity

--- app/src/main/java/com/mca/taskmaster/activities/AddTaskActivity.java
+++ app/src/main/java/com/mca/taskmaster/activities/AddTaskActivity.java
@@ -11,13 +11,18 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.amplifyframework.api.graphql.model.ModelMutation;
+import com.amplifyframework.api.graphql.model.ModelQuery;
 import com.amplifyframework.core.Amplify;
 import com.amplifyframework.datastore.generated.model.Task;
 import com.amplifyframework.datastore.generated.model.TaskStatus;
+import com.amplifyframework.datastore.generated.model.Team;
 import com.mca.taskmaster.R;
 import com.mca.taskmaster.utils.TaskStatusUtility;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 public class AddTaskActivity extends AppCompatActivity {
     public static final String TAG = "add_task_tag";
@@ -26,6 +31,31 @@ public class AddTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+        CompletableFuture<List<Team>> teamsFuture = new CompletableFuture<>();
+        List<Team> teamList = new ArrayList<>();
+        List<String> teamListAsString = new ArrayList<>();
+        Spinner taskTeamSpinner = findViewById(R.id.spinner_add_task_team);
+        Amplify.API.query(
+                ModelQuery.list(Team.class),
+                success -> {
+                    Log.i(TAG, "Read Teams successfully");
+                    for (Team team : success.getData()) {
+                        teamList.add(team);
+                    }
+                    teamsFuture.complete(teamList);
+                    runOnUiThread(() -> {
+                        for (Team team : teamList)
+                            teamListAsString.add(team.getName());
+                        taskTeamSpinner.setAdapter(new ArrayAdapter<>(
+                                this,
+                                android.R.layout.simple_spinner_item,
+                                teamListAsString
+                        ));
+                    });
+                },
+                failure -> Log.i(TAG, "Failed to read Teams")
+        );
 
         List<String> statusList = TaskStatusUtility.getTaskStatusList();
 
@@ -44,10 +74,14 @@ public class AddTaskActivity extends AppCompatActivity {
 
             TaskStatus newStatus = TaskStatusUtility.taskStatusFromString(taskStatusSpinner.getSelectedItem().toString());
 
+            String teamString = taskTeamSpinner.getSelectedItem().toString();
+            Team team = teamList.stream().filter(e -> e.getName().equals(teamString)).collect(Collectors.toList()).get(0);
+
             Task newTask = Task.builder()
                     .title(titleEditText.getText().toString())
                     .body(descriptionEditText.getText().toString())
                     .status(newStatus)
+                    .team(team)
                     .build();
 
             Amplify.API.mutate(
@@ -56,6 +90,7 @@ public class AddTaskActivity extends AppCompatActivity {
                     failure -> Log.i(TAG, "AddTaskActivity.onCreate(): failed to add a task")
             );
 
+            taskTeamSpinner.setSelection(0);
             ((EditText)findViewById(R.id.my_task_input)).setText("");
             ((EditText)findViewById(R.id.task_description_input)).setText("");
             taskStatusSpinner.setSelection(0);
